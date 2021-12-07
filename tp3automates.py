ccd7de3b8d4d64aea33c98514b6315aa2573cd89
hadrien <hadrien.stoppolani@etu.univ-amu.fr>
2021-12-07 22:51:45 +0100

    tp3

--- tp3automates.py
+++ tp3automates.py
@@ -5,57 +5,82 @@ Applies Kleene's star, concatenation and union of automata.
 
 from automaton import Automaton, EPSILON, State, error, warn
 import sys
-import pdb # for debugging
+import pdb  # for debugging
+
+
+##################
+def nouvel_etat(a1: Automaton) -> str:
+    maxstate = -1
+    for a in a1.states:
+        try:
+            maxstate = max(int(a), maxstate)
+        except ValueError:
+            pass
+    return str(maxstate + 1)
+
+
+def kleene(a1: Automaton) -> Automaton:
+    a1star = a1.deepcopy()
+    a1star.name = "a1star"
+    for index in a1star.acceptstates:
+        a1star.add_transition(a1star.acceptstates[index], EPSILON, a1star.initial.name)
+    nom_nouvel_etat = nouvel_etat(a1star)
+    a1star.add_transition(nom_nouvel_etat, EPSILON, a1star.initial.name)
+    a1star.initial = a1star.statesdict[nom_nouvel_etat]
+    a1star.make_accept(nom_nouvel_etat)
+    return a1star
+
 
 ##################
 
-def kleene(a1:Automaton)->Automaton:
-  # TODO: implement Kleene's star
-  return a1 
+def concat(a1: Automaton, a2: Automaton) -> Automaton:
+    # TODO: implement concatenation
+    return a1
+
 
 ##################
 
-def concat(a1:Automaton, a2:Automaton)->Automaton:
-  # TODO: implement concatenation
-  return a1 
+def union(a1: Automaton, a2: Automaton) -> Automaton:
+    a1_a2 = a1star.deepcopy()
+    a1_a2.name = "a1_a2"
+    nom_nouvel_etat = nouvel_etat(a1_a2)
+    for s in a2.states:
+      if s in a1_a2.states:
+        a2.rename_state(s, nom_nouvel_etat)
+    nom_nouvel_etat = str(int(nom_nouvel_etat) + 1)
+    return a1_a2
+
 
 ##################
 
-def union(a1:Automaton, a2:Automaton)->Automaton:
-  # TODO: implement union
-  return a1 
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        usagestring = "Usage: {} <automaton-file1.af> <automaton-file2.af>"
+        error(usagestring.format(sys.argv[0]))
 
-##################
+    # First automaton, argv[1]
+    a1 = Automaton("dummy")
+    a1.from_txtfile(sys.argv[1])
+    a1.to_graphviz(a1.name + ".gv")
+    print(a1)
 
-if __name__ == "__main__" :
-  if len(sys.argv) != 3:
-    usagestring = "Usage: {} <automaton-file1.af> <automaton-file2.af>"
-    error(usagestring.format(sys.argv[0]))
+    # Second automaton, argv[2]
+    a2 = Automaton("dummy")
+    a2.from_txtfile(sys.argv[2])
+    a2.to_graphviz(a2.name + ".gv")
+    print(a2)
 
-  # First automaton, argv[1]
-  a1 = Automaton("dummy")
-  a1.from_txtfile(sys.argv[1])
-  a1.to_graphviz(a1.name+".gv")
-  print(a1)
+    a1star = kleene(a1)
+    print()
+    print(a1star)
+    a1star.to_graphviz("a1star.gv")
 
-  # Second automaton, argv[2]
-  a2 = Automaton("dummy")
-  a2.from_txtfile(sys.argv[2])
-  a2.to_graphviz(a2.name+".gv")
-  print(a2)
-    
-  a1star = kleene(a1)
-  print()
-  print(a1star)
-  a1star.to_graphviz("a1star.gv")
+    a1a2 = concat(a1, a2)
+    print()
+    print(a1a2)
+    a1a2.to_graphviz("a1a2.gv")
 
-  a1a2 = concat(a1, a2)
-  print()
-  print(a1a2)
-  a1a2.to_graphviz("a1a2.gv")
-
-  a1ora2 = union(a1, a2)
-  print()
-  print(a1ora2)
-  a1ora2.to_graphviz("a1ora2.gv")
-
+    a1ora2 = union(a1, a2)
+    print()
+    print(a1ora2)
+    a1ora2.to_graphviz("a1ora2.gv")
